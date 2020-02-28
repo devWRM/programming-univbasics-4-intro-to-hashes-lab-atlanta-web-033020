2ee0780ff1959ab0a00883315bf830ac03d932f7
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-28 01:48:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  id_tag = { :id => Math.random()}
+  id_tag = { :id => rand}
 end
 
 def my_hash_creator(key, value)
